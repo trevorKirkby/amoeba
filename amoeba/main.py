903f33d952cb26f453e8c6637b9e7cd6cce51a67
David Kirkby <dkirkby@uci.edu>
2020-07-25 17:49:03 -0700

    Basic script

--- amoeba/main.py
+++ amoeba/main.py
@@ -1,1 +1,13 @@
-from yaml import load
+import pkg_resources
+import yaml
+from amoeba.core import *
+
+
+def main():
+    stream = pkg_resources.resource_stream(__name__, 'data/amoeba.yaml')
+    config = yaml.safe_load(stream)
+    print(config)
+
+
+if __name__ == "__main__":
+    main()
