e0176ca7152ae1d22a03da83a9c3f6b359507ee1
David Kirkby <dkirkby@uci.edu>
2020-07-25 17:38:42 -0700

    Create setup.py

---
+++ README.md
@@ -0,0 +1,15 @@
+# Amoeba
+
+Don't panic
+
+To install a production version:
+```
+pip install git+https://github.com/trevorKirkby/amoeba.git
+```
+
+To install a development version use:
+```
+git clone https://github.com/trevorKirkby/amoeba.git
+cd amoeba
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amoeba",
+    version="0.0.1",
+    author="Trevor & David Kirkby",
+    author_email="trevor.p.kirkby@gmail.com",
+    description="A topical game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trevorKirkby/amoeba",
+    packages=setuptools.find_packages(),
+    install_requires=["pyyaml"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
